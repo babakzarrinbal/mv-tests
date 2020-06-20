da2894be2ac13263550210c885d566f9f3be9559
babak <babak.zarrinbal@gmail.com>
2020-06-20 13:00:06 +0430

    first build

---
+++ docs/precache-manifest.a24b70845232d624fbb2db59406485ca.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed817070ce3d1237068f9584bf71c5b7",
+    "url": "/mv-tests/./manifest.json"
+  },
+  {
+    "revision": "51262f49a81e36358383",
+    "url": "/mv-tests/css/app.b79c85dc.css"
+  },
+  {
+    "revision": "9db106216a9b44169c9a",
+    "url": "/mv-tests/css/chunk-vendors.0ba3e87d.css"
+  },
+  {
+    "revision": "da8857c4b5ff5228eda7b8bad5514750",
+    "url": "/mv-tests/img/icon/downloadqueue.png"
+  },
+  {
+    "revision": "7e7fd524bff2b430af8a924bae7b6fad",
+    "url": "/mv-tests/img/icon/list.png"
+  },
+  {
+    "revision": "01711fa464361d1f2c5994e387288432",
+    "url": "/mv-tests/img/icon/notsee.png"
+  },
+  {
+    "revision": "3e7f8afa35e80c2ec94e5d93bbf47b3a",
+    "url": "/mv-tests/img/icon/see.png"
+  },
+  {
+    "revision": "39153b06929a746eee884dd665ab0dc8",
+    "url": "/mv-tests/img/icon/setting.png"
+  },
+  {
+    "revision": "536ef82528266ef7716cc76f13c960e4",
+    "url": "/mv-tests/img/loaders/movie.gif"
+  },
+  {
+    "revision": "d8f8be8601d46e34d280ea4e44a5be1d",
+    "url": "/mv-tests/img/loaders/puff.svg"
+  },
+  {
+    "revision": "e3eca74d0d77d7299725c27949c59ebd",
+    "url": "/mv-tests/img/loaders/rings.svg"
+  },
+  {
+    "revision": "f7caff4f185c623ba45c02ab832f9c1f",
+    "url": "/mv-tests/img/ttsicons/bookmark.svg"
+  },
+  {
+    "revision": "783c7836eb439161b1f9fa84430c7bf9",
+    "url": "/mv-tests/img/ttsicons/list.svg"
+  },
+  {
+    "revision": "20ca233e48ad5b5a127f6f1d86a4e4ef",
+    "url": "/mv-tests/img/ttsicons/minus.svg"
+  },
+  {
+    "revision": "85a5675b8b7737090914d26418d8175d",
+    "url": "/mv-tests/img/ttsicons/next-w.svg"
+  },
+  {
+    "revision": "3a1311d5e71f7d0d9cf75005f1992426",
+    "url": "/mv-tests/img/ttsicons/next.svg"
+  },
+  {
+    "revision": "2d862f5aaa34fed9e533fbfc84d4080e",
+    "url": "/mv-tests/img/ttsicons/pause-w.svg"
+  },
+  {
+    "revision": "14b4111eac1e3e8aeaaee5e2d9685dc6",
+    "url": "/mv-tests/img/ttsicons/pause.svg"
+  },
+  {
+    "revision": "6ee7698493609851c684cc8b560084a7",
+    "url": "/mv-tests/img/ttsicons/play-full.svg"
+  },
+  {
+    "revision": "eb811774ab3b6bec73673e8ec3c24105",
+    "url": "/mv-tests/img/ttsicons/play-w.svg"
+  },
+  {
+    "revision": "7db92e62bee29459989694fb4526bc16",
+    "url": "/mv-tests/img/ttsicons/play.svg"
+  },
+  {
+    "revision": "8181039b61cfc565659fa059c039e706",
+    "url": "/mv-tests/img/ttsicons/plus.svg"
+  },
+  {
+    "revision": "ce554f9aa15080bcf5d30af0549b0779",
+    "url": "/mv-tests/img/ttsicons/refresh.svg"
+  },
+  {
+    "revision": "50acadf632bc9506653499fff9bb30ac",
+    "url": "/mv-tests/img/ttsicons/settings.svg"
+  },
+  {
+    "revision": "72d4a6cd86f52d8d64c4eefb089542cb",
+    "url": "/mv-tests/img/ttsicons/shield.svg"
+  },
+  {
+    "revision": "35bd9c73467856403ded3fb374a1bd5a",
+    "url": "/mv-tests/index.html"
+  },
+  {
+    "revision": "51262f49a81e36358383",
+    "url": "/mv-tests/js/app.0f633b43.js"
+  },
+  {
+    "revision": "9db106216a9b44169c9a",
+    "url": "/mv-tests/js/chunk-vendors.e4db6e85.js"
+  },
+  {
+    "revision": "e8ff09c5bc760608e7431f14032fd293",
+    "url": "/mv-tests/logo.png"
+  },
+  {
+    "revision": "1a27483ec99b4e5277b3b6878f8355fb",
+    "url": "/mv-tests/manifest.json"
+  }
+]);
